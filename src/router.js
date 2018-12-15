7dbcfd4d3fb11cde7a6feffa99928e32d52a978a
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-15 00:14:39 -0500

    Lazy load pages

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
+
+const About = () => import('@/views/About');
+const Home = () => import('@/views/Home');
 
 export default new Router({
   routes: [
@@ -13,11 +15,7 @@ export default new Router({
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: About,
     },
   ],
 });
