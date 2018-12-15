fdc906ffe6d01522dee3093fcbf7fe070630adb8
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-15 00:21:56 -0500

    Setup Vuex to store links

--- src/store.js
+++ src/store.js
@@ -5,10 +5,16 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    links: [],
   },
   mutations: {
-
+    // link contains the website name + url
+    addLink: (state, link) => {
+      state.links.push({ Website: link['0'], Url: link['1'] });
+    },
+  },
+  getters: {
+    getLinks: state => state.links,
   },
   actions: {
 
