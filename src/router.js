c06113634a37b31a1275e42520938977a19eed26
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-21 14:19:27 -0500

    Kill dead code

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import HeroTitle from '@/components/HeroTitle.vue';
 import NavButtonRow from '@/components/NavButtonRow.vue';
-import ContentWindow from '@/components/ContentWindow.vue';
 import ContentWindowB from '@/components/ContentWindowB.vue';
 import Particles from '@/components/Particles.vue';
 import UnderConstruction from '@/components/UnderConstruction.vue';
@@ -21,18 +20,6 @@ export default new Router({
       name: 'home',
       component: Home,
       children: [
-
-        {
-          path: '/welcome',
-          name: 'WelcomeMessage',
-          components: {
-            a: HeroTitle,
-            b: ContentWindow,
-            d: NavButtonRow,
-
-          },
-        },
-
         {
           path: '',
           name: 'HomePage',
