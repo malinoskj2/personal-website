d94bb6b4eaed37768ea60f699f36ddd443e36785
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-20 22:17:02 -0500

    Add EventItem component

---
+++ src/components/EventItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <p> </p>
+
+</template>
+
+<script>
+export default {
+  name: 'EventItem',
+  props: {
+    event: Object,
+  },
+  data() {
+    return { };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
