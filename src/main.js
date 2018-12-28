cc8f0bce9ac1031c855bf86407b7d272db036710
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-28 18:30:16 -0500

    Install vue-sequential-entrace

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue';
 import VueParticles from 'vue-particles';
 import VueTyperPlugin from 'vue-typer';
 
+import SequentialEntrance from 'vue-sequential-entrance';
+import 'vue-sequential-entrance/vue-sequential-entrance.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(SequentialEntrance);
 Vue.use(VueParticles);
 Vue.use(VueTyperPlugin);
 
