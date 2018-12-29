07e8f7ff5493c44a88dec2a52db60ede83650a92
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-28 19:08:40 -0500

    Add sequential animations to projects

--- src/components/ProjectTiles.vue
+++ src/components/ProjectTiles.vue
@@ -2,7 +2,7 @@
 
     <div class="section">
 
-      <sequential-entrance fromBottom>
+      <sequential-entrance animation="fadeInUp" delay="100">
         <div class="j-project"
              v-for="(project) in getProjects"
              :key="project.name" >
@@ -62,6 +62,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+  @-webkit-keyframes fadeInUp {
+    from {
+      opacity: 0;
+      -webkit-transform:  translate3d(0, 100%, 0);
+      transform:  translate3d(0, 100%, 0);
+    }
+    to {
+      opacity: 1;
+      -webkit-transform: translate3d(0, 0, 0);
+      transform:  translate3d(0, 0, 0);
+    }
+  }
+
+  @keyframes fadeInUp {
+    from {
+      opacity: 0;
+      -webkit-transform:  translate3d(0, 100%, 0);
+      transform: translate3d(0, 100%, 0);
+    }
+    to {
+      opacity: 1;
+      -webkit-transform:  translate3d(0, 0, 0);
+      transform:   translate3d(0, 0, 0);
+    }
+  }
+
+  .fadeInUp {
+    -webkit-animation: fadeInUp 4s cubic-bezier(0.455, 0.030, 0.515, 0.955);
+    animation: fadeInUp .4s cubic-bezier(0.455, 0.030, 0.515, 0.955);
+  }
+
+
+  .testAnim {
+    transform: scale(2);
+  }
+
   .lrgbox-trans-enter-active {
     filter: opacity(0);
     transform: translateY(-10%);
@@ -73,17 +110,30 @@ export default {
     animation: bounce-in .4s reverse;
   }
 
-  @keyframes bounce-in {
+  @keyframes jenter {
     0% {
-      filter:opacity(0);
+
       transform: translateY(32%) scale(.8);
     }
     50% {
-      filter: opacity(.5);
+
       transform: translateY(16%) scale(1.1);
     }
     100% {
-      filter: opacity(1);
+      transform: translateY(0%) scale(1);
+    }
+  }
+
+  @-webkit-keyframes jenter {
+    0% {
+
+      transform: translateY(32%) scale(.8);
+    }
+    50% {
+
+      transform: translateY(16%) scale(1.1);
+    }
+    100% {
       transform: translateY(0%) scale(1);
     }
   }
