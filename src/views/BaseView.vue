413e261b71419222ee8dae63a0951159f1614f65
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-30 15:37:08 -0500

    Add footer

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -9,6 +9,18 @@
       </div>
     </section>
 
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p id="thank-you"><strong>thanks for visting my site</strong></p>
+        <p>
+          Copyright <strong>©</strong> 2018 Jesse Malinosky. The source code is licensed
+          <a href="https://opensource.org/licenses/BSD-2-Clause">BSD 2-Clause</a>.
+          The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
+
   </div>
 </template>
 
@@ -33,5 +45,12 @@ export default {
 };
 </script>
 <style>
-
+  .footer {
+    margin: .5rem;
+    background: rgba(214, 248, 214, 0.47);
+  }
+  #thank-you {
+    font-family: Karla;
+    font-size: .75rem;
+  }
 </style>
