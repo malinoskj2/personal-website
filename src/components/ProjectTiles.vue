84f0715b16ae61d90afaf6c798230f1fbca9896b
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-31 18:45:21 -0500

    Cleanup ProjectTiles.vue

--- src/components/ProjectTiles.vue
+++ src/components/ProjectTiles.vue
@@ -2,31 +2,21 @@
 
       <div class="section">
 
-        <div class="columns">
-          <div class="column">
-            <div class="field is-grouped is-grouped-multiline">
-
-
-            </div>
-          </div>
-        </div>
-
-
             <div  class="columns"
                   :style="{'min-height': dynamicMin +'px'}"
             ref="projectList">
               <div  class="column is-8">
-                <sequential-entrance animation="fadeInUp" delay="100">
+                <sequential-entrance animation="fadeInUp" delay="200">
                 <div class="j-project"
                      v-for="(project) in projectsFiltered"
                      :key="project.name" >
 
                 <div class="columns">
                   <div class="column is-12">
-                    <transition appear name="lrgbox-trans">
+
                       <ProjectDescripBox
                         :projectInfo="project"/>
-                    </transition>
+
                   </div>
                 </div>
               </div>
@@ -108,20 +98,6 @@ export default {
     }
   }
 
-
-  @-webkit-keyframes fadeInUp {
-    from {
-      opacity: 0;
-      -webkit-transform:  translate3d(0, 100%, 0);
-      transform:  translate3d(0, 100%, 0);
-    }
-    to {
-      opacity: 1;
-      -webkit-transform: translate3d(0, 0, 0);
-      transform:  translate3d(0, 0, 0);
-    }
-  }
-
   @keyframes fadeInUp {
     from {
       opacity: 0;
@@ -140,47 +116,4 @@ export default {
     animation: fadeInUp .2s cubic-bezier(0.455, 0.030, 0.515, 0.955);
   }
 
-
-  .testAnim {
-    transform: scale(2);
-  }
-
-  .lrgbox-trans-enter-active {
-    filter: opacity(0);
-    transform: translateY(-10%);
-    animation: bounce-in -.2s .4s forwards;
-  }
-  .lrgbox-trans-leave-active {
-    filter: opacity(0);
-    transform: translateY(-10%);
-    animation: bounce-in .4s reverse;
-  }
-
-  @keyframes jenter {
-    0% {
-
-      transform: translateY(32%) scale(.8);
-    }
-    50% {
-
-      transform: translateY(16%) scale(1.1);
-    }
-    100% {
-      transform: translateY(0%) scale(1);
-    }
-  }
-
-  @-webkit-keyframes jenter {
-    0% {
-
-      transform: translateY(32%) scale(.8);
-    }
-    50% {
-
-      transform: translateY(16%) scale(1.1);
-    }
-    100% {
-      transform: translateY(0%) scale(1);
-    }
-  }
 </style>
