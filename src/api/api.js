5e0e6be0bd84b42b29765407fa05b8b583e89633
Jesse Malinosky <j2@jessemalinosky.com>
2018-12-31 20:29:49 -0500

    print error message if bad response to get request

--- src/api/api.js
+++ src/api/api.js
@@ -5,16 +5,22 @@ const api = axios.create({
 });
 
 export const getRecentCommits = async () => {
-  const commit = await api.get('/commits');
+  const commit = await api.get('/commits')
+    .catch(error => console.log(error));
+
   return commit.data;
 };
 
 export const getRepos = async () => {
-  const repos = await api.get('/repos');
+  const repos = await api.get('/repos')
+    .catch(error => console.log(error));
+
   return repos.data;
 };
 
 export const getStatuses = async () => {
-  const statuses = api.get('/statuses');
+  const statuses = api.get('/statuses')
+    .catch(error => console.log(error));
+
   return statuses.data;
 };
